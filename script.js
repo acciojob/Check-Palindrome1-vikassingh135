eee8d648d84759e9282c7bb2588f9b035bdee8c6
Vikas Singh <69408149+vikassingh135@users.noreply.github.com>
2023-05-13 09:50:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 function palindrome(str){
 
-	str = String.toLowerCase(str);
+
 	let flag = true;
 	
-	for(let i=0; i<str.length/2; i++) {
+	for(let i=0; i<=parseInt(str.length/2); i++) {
 		if(str[i] != str[str.length-1-i]) {
 			flag = false;
 			break;
