d8cf114e306619336c5072b95ad8a825f6af838f
Vikas Singh <69408149+vikassingh135@users.noreply.github.com>
2023-05-13 09:36:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,15 @@
 
 function palindrome(str){
 
+	let flag = true;
+	for(let i=0; i<str.length/2; i++) {
+		if(str[i] != str[str.length-1-i]) {
+			flag = false;
+			break;
+		}
+	}
+
+	return flag;
+
 }
 module.exports = palindrome
