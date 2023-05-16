e55b9a76523adde7d9715a81b66d15ceaf9df6bd
Vikas Singh <69408149+vikassingh135@users.noreply.github.com>
2023-05-16 22:14:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ function palindrome(str){
 
 
 	let flag = true;
-	
+    str = str.toLowerCase();  	
 	for(let i=0; i<=parseInt(str.length/2); i++) {
 		if(str[i] != str[str.length-1-i]) {
 			flag = false;
 			break;
 		}
 	}
-
 	return flag;
 }
 module.exports = palindrome
